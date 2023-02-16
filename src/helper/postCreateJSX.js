d89b33633c401b35e24edc256636eb3fecd42c28
Sergo <46654316+s3rxio@users.noreply.github.com>
2023-02-17 00:27:04 +0300

    Update postCreateJSX.js

--- src/helper/postCreateJSX.js
+++ src/helper/postCreateJSX.js
@@ -1,11 +1,9 @@
 export default function postCreateJSX({times, element}){
-    if (times) {
-        let jsx = [];
-        for (let index = 0; index < times; index++) {
-          jsx.push(element);
-        }
-        return jsx;
-      } else {
-        return element;
-      }
+    if (!times) return element;
+    
+    let jsx = [];
+    for (let index = 0; index < times; index++) {
+      jsx.push(element);
+    }
+    return jsx;
 }
