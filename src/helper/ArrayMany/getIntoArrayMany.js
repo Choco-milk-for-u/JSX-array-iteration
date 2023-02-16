5969897ae873c7173c2d5dde520611e55c23bc39
Sergo <46654316+s3rxio@users.noreply.github.com>
2023-02-17 00:16:32 +0300

    Update getIntoArrayMany.js

--- src/helper/ArrayMany/getIntoArrayMany.js
+++ src/helper/ArrayMany/getIntoArrayMany.js
@@ -2,32 +2,32 @@ import startMapping from "../getIntoArray/startMapping";
 
 export default function getIntoArrayMany(staticValue, options) {
   let data = null;
-  if (staticValue) {
-    const returnArray = Array.isArray(staticValue) ? staticValue : false;
-    const searchKey = options.key.searchKey;
-    const searchTimes = options.renderTimes.searchTimes;
-    if (searchKey || searchTimes) {
-      const intoMapping = returnArray || Object.entries(staticValue);
-      const deepClone = JSON.parse(JSON.stringify(intoMapping));
+  
+  if (!staticValue) return;
+ 
+  const returnArray = Array.isArray(staticValue) ? staticValue : false;
+  const searchKey = options.key.searchKey;
+  const searchTimes = options.renderTimes.searchTimes;
 
-      const optionsTimes = [options.key.searchKey, options.renderTimes.searchTimes];
-      const type = typeof staticValue === "object" && !returnArray ? "object" : "array";
-      let currentData = null;
-      for (let index = 0; index < optionsTimes.length; index++) {
-        const current = optionsTimes[index];
-        if (current) {
-          currentData = startMapping({
-            neededKey: current,
-            data: currentData ? currentData : deepClone,
-            type: type,
-            firstLayer: true,
-          });
-        }
-      }
-      data = currentData;
-    } else {
-      data = returnArray || Object.values(staticValue);
-    }
+  if (!searchKey || !searchTimes) return data = returnArray || Object.values(staticValue);
+
+  const intoMapping = returnArray || Object.entries(staticValue);
+  const deepClone = JSON.parse(JSON.stringify(intoMapping));
+
+  const optionsTimes = [options.key.searchKey, options.renderTimes.searchTimes];
+  const type = typeof staticValue === "object" && !returnArray ? "object" : "array";
+  let currentData = null;
+  for (let index = 0; index < optionsTimes.length; index++) {
+    const current = optionsTimes[index];
+    if (current) return;
+    currentData = startMapping({
+      neededKey: current,
+      data: currentData ? currentData : deepClone,
+      type: type,
+      firstLayer: true,
+    });
   }
+  data = currentData;
+  
   return data;
 }
