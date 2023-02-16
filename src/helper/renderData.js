54b3e0ed23e06fcdbb5ce0018b8a4a644f3c6ecb
Sergo <46654316+s3rxio@users.noreply.github.com>
2023-02-17 00:26:10 +0300

    Update renderData.js

--- src/helper/renderData.js
+++ src/helper/renderData.js
@@ -1,11 +1,9 @@
 export default function renderData(jsx) {
-  if (jsx.length > 0) {
-    let current = [];
-    for (let index = 0; index < jsx.length; index++) {
-      current.push(jsx[index]);
-    }
-    return current;
-  } else {
-    return null;
+  if (jsx.length < 0) return null;
+  
+  let current = [];
+  for (let index = 0; index < jsx.length; index++) {
+    current.push(jsx[index]);
   }
+  return current;
 }
