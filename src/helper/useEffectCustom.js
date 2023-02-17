1b0cd450b38b7ba666352ff0e872c88508634e1a
Sergo <cergotabagua222@gmail.com>
2023-02-17 16:08:37 +0300

    Update useEffectCustom.js

--- src/helper/useEffectCustom.js
+++ src/helper/useEffectCustom.js
@@ -4,63 +4,43 @@ import indexForJSX from "./options/indexForJSX";
 import none from "./options/none";
 
 export function useEffectCustom({
-  startWithIndex,
-  staticValue,
-  times,
-  Block,
-  props,
-  children,
-  options,
-  setJSX,
-  many = false,
+	startWithIndex,
+	staticValue,
+	times,
+	Block,
+	props,
+	children,
+	options,
+	setJSX,
+	many = false
 }) {
-  let data = null;
-  useEffect(() => {
-    const { roof, intoArray } = ifMany({
-      many,
-      Block,
-      staticValue,
-      options,
-      times,
-    });
-    if (!staticValue) {
-      data = none({
-        roof,
-        startWithIndex,
-        Block,
-        props,
-        children,
-        options,
-        intoArray,
-        staticValue,
-      });
-    } else {
-      if (!options) {
-        data = indexForJSX({
-          isTrue: true,
-          startWithIndex,
-          Block,
-          children,
-          intoArray,
-          roof,
-          props,
-          options,
-          staticValue,
-        });
-      } else {
-        data = indexForJSX({
-          isTrue: options.indexForJSX,
-          startWithIndex,
-          Block,
-          children,
-          intoArray,
-          roof,
-          props,
-          options,
-          staticValue,
-        });
-      }
-    }
-    return () => setJSX((prev) => [...prev, data]);
-  }, []);
+	useEffect(() => {
+		const { roof, intoArray } = ifMany({
+			many,
+			Block,
+			staticValue,
+			options,
+			times
+		});
+
+		const args = {
+			startWithIndex,
+			Block,
+			props,
+			children,
+			intoArray,
+			roof,
+			options,
+			staticValue
+		};
+
+		const data = staticValue
+			? indexForJSX({
+					isTrue: options ? options.indexForJSX : true,
+					...args
+			  })
+			: none(args);
+
+		return () => setJSX(prev => [...prev, data]);
+	}, []);
 }
